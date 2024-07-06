1033020fe775e4afbbfe5c37179f6c7b5512729b
marcandreher <herpersmarcandre3@gmail.com>
2024-07-06 16:11:49 +0200

    fix: value diff

--- src/main/java/osu/serverlist/Sites/Endpoints/ChartDataRoute.java
+++ src/main/java/osu/serverlist/Sites/Endpoints/ChartDataRoute.java
@@ -11,7 +11,7 @@ import spark.Response;
 
 public class ChartDataRoute extends JsonProcessingRoute {
     public final String CHECKFORCRAWLSTAT_SQL = "SELECT DAYOFWEEK(d.date) AS `date`, IFNULL(`value`, 0) AS value FROM (SELECT CURDATE() - INTERVAL 6 DAY AS date UNION SELECT CURDATE() - INTERVAL 5 DAY UNION SELECT CURDATE() - INTERVAL 4 DAY UNION SELECT CURDATE() - INTERVAL 3 DAY UNION SELECT CURDATE() - INTERVAL 2 DAY UNION SELECT CURDATE() - INTERVAL 1 DAY UNION SELECT CURDATE()) AS d LEFT JOIN un_crawler v ON DATE(v.date) = d.date AND v.srv_id = ? AND `type` = ? ORDER BY d.date;";
-    public final String CHECKFORCRAWLSTATDIFF_SQL = "SELECT DAYOFWEEK(d.date) AS `date`, IFNULL(v1.value, 0) - IFNULL(v2.value, 0) AS value FROM (SELECT CURDATE() - INTERVAL 6 DAY AS date UNION SELECT CURDATE() - INTERVAL 5 DAY UNION SELECT CURDATE() - INTERVAL 4 DAY UNION SELECT CURDATE() - INTERVAL 3 DAY UNION SELECT CURDATE() - INTERVAL 2 DAY UNION SELECT CURDATE() - INTERVAL 1 DAY UNION SELECT CURDATE()) AS d LEFT JOIN un_crawler v1 ON DATE(v1.date) = d.date AND v1.srv_id = 36 AND v1.type = ? LEFT JOIN un_crawler v2 ON DATE(v2.date) = d.date - INTERVAL 1 DAY AND v2.srv_id = ? AND v2.type = ? ORDER BY d.date;";
+    public final String CHECKFORCRAWLSTATDIFF_SQL = "SELECT DAYOFWEEK(d.date) AS `date`, IFNULL(v1.value, 0) - IFNULL(v2.value, 0) AS value FROM (SELECT CURDATE() - INTERVAL 6 DAY AS date UNION SELECT CURDATE() - INTERVAL 5 DAY UNION SELECT CURDATE() - INTERVAL 4 DAY UNION SELECT CURDATE() - INTERVAL 3 DAY UNION SELECT CURDATE() - INTERVAL 2 DAY UNION SELECT CURDATE() - INTERVAL 1 DAY UNION SELECT CURDATE()) AS d LEFT JOIN un_crawler v1 ON DATE(v1.date) = d.date AND v1.srv_id = ? AND v1.type = ? LEFT JOIN un_crawler v2 ON DATE(v2.date) = d.date - INTERVAL 1 DAY AND v2.srv_id = ? AND v2.type = ? ORDER BY d.date;";
 
     public ChartDataRoute() {
         super();
@@ -27,7 +27,7 @@ public class ChartDataRoute extends JsonProcessingRoute {
         if(request.queryParams("type").equalsIgnoreCase("PLAYERCHECK")) {
             serverResultSet = mysql.Query(CHECKFORCRAWLSTAT_SQL, request.queryParams("id"), request.queryParams("type"));
         }else{
-            serverResultSet = mysql.Query(CHECKFORCRAWLSTATDIFF_SQL, request.queryParams("type"), request.queryParams("id"), request.queryParams("type"));
+            serverResultSet = mysql.Query(CHECKFORCRAWLSTATDIFF_SQL, request.queryParams("id"), request.queryParams("type"), request.queryParams("id"), request.queryParams("type"));
         }
         
         List<ChartData> data = new ArrayList<>();
